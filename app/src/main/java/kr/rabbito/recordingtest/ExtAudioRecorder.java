c2a9afbc5413bbcf2561cdc79b3d21f88bfd9dc9
ThinkingDobby <chiho1221@naver.com>
2022-07-18 20:31:41 +0900

    라이센스 고지 추가

--- app/src/main/java/kr/rabbito/recordingtest/ExtAudioRecorder.java
+++ app/src/main/java/kr/rabbito/recordingtest/ExtAudioRecorder.java
@@ -11,6 +11,25 @@ import android.media.MediaRecorder;
 import android.media.MediaRecorder.AudioSource;
 import android.util.Log;
 
+/**
+ * Copyright (C) 2010-2012 Magnus Raaum, Lars Moland Eliassen, Christoffer Jun Marcussen, Rune Sætre
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * README:
+ *
+ */
+
 public class ExtAudioRecorder
 {
     private final static int[] sampleRates = {44100, 22050, 11025, 8000};
